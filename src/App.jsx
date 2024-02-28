07b4124ccecd9f556f782ca2af516cd86b24daf9
Vans <53773587+iamfernweh@users.noreply.github.com>
2024-02-28 12:48:38 -0500

    updates

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import { Routes, Route, Link } from 'react-router-dom';
 import './App.css';
 
 function App() {
@@ -8,7 +9,12 @@ function App() {
       <div id="container">
         <h1>Hello React Router!</h1>
         <div id="navbar">{/* navigation here */}</div>
-        <div id="main-section">{/* routes here */}</div>
+        <div id="main-section">
+          <Routes>
+            <Route path='/blue' element={<h1>Blue</h1>} />
+            <Route path='/red' element={<h1>Red</h1>} />
+          </Routes>
+        </div>
       </div>
     </>
   );
